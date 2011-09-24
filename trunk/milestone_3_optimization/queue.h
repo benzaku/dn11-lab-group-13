526e6e387435eed4764f093fa5b40e2f90f749aa
cn.jihao@gmail.com <cn.jihao@gmail.com@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-09-24 22:00:00 +0000

    initial milestone3_optimization

---
+++ trunk/milestone_3_optimization/queue.h
@@ -0,0 +1,40 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#include <cnet.h>
+
+struct elemType
+{
+	int link;
+	size_t length;
+	char *packet;
+};
+struct sNode{
+    struct elemType data;
+    struct sNode *next;
+};
+struct queueLK{
+    struct sNode *front;
+    struct sNode *rear;
+    int size;
+};
+
+
+extern void initQueue(struct queueLK *hq);
+
+
+extern void enQueue(struct queueLK *hq, struct elemType x);
+
+
+extern struct elemType outQueue(struct queueLK *hq);
+
+
+extern struct elemType peekQueue(struct queueLK *hq);
+
+
+extern int emptyQueue(struct queueLK *hq);
+
+
+extern void clearQueue(struct queueLK *hq);
+
+#endif
