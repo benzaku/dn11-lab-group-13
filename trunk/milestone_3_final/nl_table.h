35d68c2a940176bdbfc42750002d5f0b1e5d6102
cn.jihao@gmail.com <cn.jihao@gmail.com@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-09-25 19:47:25 +0000

    initial milestone_3_final

---
+++ trunk/milestone_3_final/nl_table.h
@@ -0,0 +1,22 @@
+#ifndef _NL_TABLE_H_
+#define _NL_TABLE_H_
+
+#include <cnet.h>
+
+#define	ALL_LINKS	(-1)
+
+extern	void	reboot_NL_table(void);
+
+extern	int	NL_ackexpected(CnetAddr address);
+extern	int	NL_nextpackettosend(CnetAddr address);
+extern	int	NL_packetexpected(CnetAddr address);
+
+extern	void	inc_NL_packetexpected(CnetAddr address);
+extern	void	inc_NL_ackexpected(CnetAddr address);
+
+extern	int	NL_linksofminhops(CnetAddr address);
+extern	void	NL_savehopcount(CnetAddr address, int trans_time, int link);
+
+extern	int	NL_get_has_resent(CnetAddr address);
+extern	void	NL_set_has_resent(CnetAddr address, unsigned short int value);
+#endif
---
+++ trunk/milestone_3_final/queue.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+void initQueue(struct queueLK *hq) {
+	hq->front = hq->rear = NULL;
+	hq->size = 0;
+	return;
+}
+
+void enQueue(struct queueLK *hq, struct elemType x) {
+	struct sNode *newP;
+	newP = malloc(sizeof(struct sNode));
+	if (newP == NULL) {
+		printf("Fail to allocate memory");
+		exit(1);
+	}
+	newP->data = x;
+	newP->next = NULL;
+	if (hq->rear == NULL) {
+		hq->front = hq->rear = newP;
+	} else {
+		hq->rear = hq->rear->next = newP;
+	}
+	hq->size++;
+
+	return;
+}
+
+struct elemType outQueue(struct queueLK *hq) {
+	struct sNode *p;
+	struct elemType temp;
+	if (hq->front == NULL) {
+		printf("Empty queue!");
+		exit(1);
+	}
+	temp = hq->front->data;
+	p = hq->front;
+	hq->front = p->next;
+	if (hq->front == NULL) {
+		hq->rear = NULL;
+	}
+	free(temp.packet);
+	free(p);
+	hq->size--;
+	return temp;
+}
+
+struct elemType peekQueue(struct queueLK *hq) {
+	if (hq->front == NULL) {
+		printf("Empty queue!");
+		exit(1);
+	}
+	return hq->front->data;
+}
+
+
+int emptyQueue(struct queueLK *hq) {
+
+	if (hq->front == NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+
+void clearQueue(struct queueLK *hq) {
+	struct sNode *p = hq->front;
+
+	while (p != NULL) {
+		hq->front = hq->front->next;
+		free(p);
+		p = hq->front;
+	}
+	hq->rear = NULL;
+	hq->size = 0;
+	return;
+}
