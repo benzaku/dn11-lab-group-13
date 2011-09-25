c8786ff5f2b6002ff952329604b4511ec76234bd
benzaku <benzaku@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-09-25 22:36:24 +0000

    formatting milestone3.h

--- milestone_3_final/milestone3.h
+++ milestone_3_final/milestone3.h
@@ -1,4 +1,3 @@
- 
 #ifndef _MILESTONE3_H_
 #define _MILESTONE3_H_
 
@@ -14,14 +13,15 @@ static char *rb[256];
 NL_PACKET * lastPacket;
 static int mtu;
 
-extern void send_ack(NL_PACKET *p, int arrived_on_link, unsigned short int is_err_ack);
+extern void send_ack(NL_PACKET *p, int arrived_on_link,
+		unsigned short int is_err_ack);
 extern void print_msg(char *msg, size_t length);
 extern void update_last_packet(NL_PACKET *last);
 extern NL_PACKET * get_last_packet(CnetAddr address);
-extern void sdown_pieces_to_datalink(char *packet, size_t length, int choose_link);
+extern void sdown_pieces_to_datalink(char *packet, size_t length,
+		int choose_link);
 extern int up_to_network(char *packet, size_t length, int arrived_on_link);
 extern void up_to_application(NL_PACKET *p, int arrived_on_link);
 extern void route_packet(NL_PACKET *p, int arrived_on_link);
-
 
 #endif
