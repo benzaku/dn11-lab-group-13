52136fca385a8ba7ff2f1ba3234332aa1a80ad2f
cn.jihao@gmail.com <cn.jihao@gmail.com@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-09-27 08:32:47 +0000

    restore nl_table.c nl_table.h, signal 11 exists

--- trunk/milestone_3_final2/nl_table_bak.c
+++ trunk/milestone_3_final2/nl_table_bak.c
@@ -6,33 +6,11 @@
 #include "nl_table.h"
 #include "nl_packet.h"
 
-// ---- A SIMPLE NETWORK LAYER SEQUENCE TABLE AS AN ABSTRACT DATA TYPE ----
-
-typedef struct {
-	CnetAddr address; // ... of remote node
-	int ackexpected; // packet sequence numbers to/from node
-	int nextpackettosend;
-	int packetexpected;
-	//int minhops; // minimum known hops to remote node
-	int minhop_trans_time;
-	int minhop_link; // link via which minhops path observed
-	NL_PACKET lastpacket;
-	unsigned short int has_resent;
-//unsigned short int resent_times; //for debug
-} NLTABLE;
-
-static NLTABLE *NL_table = NULL;
-static int NL_table_size = 0;
-static int NL_table_capacity = 0;
-static int NL_table_increment = 10;
-
-// -----------------------------------------------------------------
-
 //  GIVEN AN ADDRESS, LOCATE OR CREATE ITS ENTRY IN THE NL_table
-static int find_address(CnetAddr address) {
+static int find_index(CnetAddr src, CnetAddr dest) {
 	//  ATTEMPT TO LOCATE A KNOWN ADDRESS
 	for (int t = 0; t < NL_table_size; ++t)
-		if (NL_table[t].address == address)
+		if (NL_table[t].src == src && NL_table[NL_table_size].dest == dest)
 			return t;
 
 	//  UNNOWN ADDRESS, SO WE MUST CREATE AND INITIALIZE A NEW ENTRY
@@ -43,7 +21,8 @@ static int find_address(CnetAddr address) {
 				* sizeof(NLTABLE));
 		NL_table_capacity += NL_table_increment;
 	}
-	NL_table[NL_table_size].address = address;
+	NL_table[NL_table_size].src = src;
+	NL_table[NL_table_size].dest = dest;
 	//NL_table[NL_table_size].minhops = INT_MAX;
 	NL_table[NL_table_size].minhop_trans_time = INT_MAX;
 	NL_table[NL_table_size].has_resent = 0;
@@ -51,29 +30,37 @@ static int find_address(CnetAddr address) {
 	return NL_table_size++;
 }
 
-int NL_ackexpected(CnetAddr address) {
-	int t = find_address(address);
+unsigned short int NL_ackexpected(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
 	return NL_table[t].ackexpected;
 }
 
-void inc_NL_ackexpected(CnetAddr address) {
-	int t = find_address(address);
+void inc_NL_ackexpected(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
 	NL_table[t].ackexpected++;
 }
 
-int NL_nextpackettosend(CnetAddr address) {
-	int t = find_address(address);
-	return NL_table[t].nextpackettosend++;
+unsigned short int NL_nextpackettosend(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
+	int nextpackettosend = NL_table[t].nextpackettosend;
+	if(nextpackettosend == 9999)
+	  NL_table[t].nextpackettosend = 0;
+	else 
+	  NL_table[t].nextpackettosend++;
+	return nextpackettosend;
 }
 
-int NL_packetexpected(CnetAddr address) {
-	int t = find_address(address);
+unsigned short int NL_packetexpected(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
 	return NL_table[t].packetexpected;
 }
 
-void inc_NL_packetexpected(CnetAddr address) {
-	int t = find_address(address);
-	NL_table[t].packetexpected++;
+void inc_NL_packetexpected(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
+	if (NL_table[t].packetexpected == 9999)
+	  NL_table[t].packetexpected = 0;
+	else 
+	  NL_table[t].packetexpected++;
 }
 
 // -----------------------------------------------------------------
@@ -81,16 +68,16 @@ void inc_NL_packetexpected(CnetAddr address) {
 //  FIND THE LINK ON WHICH PACKETS OF MINIMUM HOP COUNT WERE OBSERVED.
 //  IF THE BEST LINK IS UNKNOWN, WE RETURN ALL_LINKS.
 
-int NL_linksofminhops(CnetAddr address) {
-	int t = find_address(address);
+int NL_linksofminhops(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
 	int link = NL_table[t].minhop_link;
 	return (link == 0) ? ALL_LINKS : (1 << link);
 }
 
 static bool given_stats = false;
 
-void NL_savehopcount(CnetAddr address, int trans_time, int link) {
-	int t = find_address(address);
+void NL_savehopcount(CnetAddr src, CnetAddr dest, int trans_time, int link) {
+	int t = find_index(src, dest);
 	if (NL_table[t].minhop_trans_time > trans_time) {
 		NL_table[t].minhop_trans_time = trans_time;
 		NL_table[t].minhop_link = link;
@@ -104,31 +91,32 @@ void NL_savehopcount(CnetAddr address, int trans_time, int link) {
 	 }*/
 }
 
-int NL_get_has_resent(CnetAddr address) {
-	int t = find_address(address);
+int NL_get_has_resent(CnetAddr src, CnetAddr dest) {
+	int t = find_index(src, dest);
 	return NL_table[t].has_resent;
 }
 
-void NL_set_has_resent(CnetAddr address, unsigned short int value) {
-	int t = find_address(address);
+void NL_set_has_resent(CnetAddr src, CnetAddr dest, unsigned short int value) {
+	int t = find_index(src, dest);
 	NL_table[t].has_resent = value;
 }
 
 /* for debug */
 /*
- void NL_inc_resent_times(CnetAddr address){
- int t = find_address(address);
+ void NL_inc_resent_times(CnetAddr src){
+ int t = find_src(src);
  NL_table[t].resent_times++;
  }
 
- unsigned short int NL_get_resent_times(CnetAddr address){
- int t = find_address(address);
+ unsigned short int NL_get_resent_times(CnetAddr src){
+ int t = find_src(src);
  return NL_table[t].resent_times;
  }
  */
 /* end debug */
 // -----------------------------------------------------------------
 
+/*
 static EVENT_HANDLER( show_NL_table) {
 	CNET_clear();
 	printf("\n%12s %12s %12s %12s", "destination", "ackexpected",
@@ -139,8 +127,8 @@ static EVENT_HANDLER( show_NL_table) {
 	printf("\n");
 
 	for (int t = 0; t < NL_table_size; ++t)
-		if (NL_table[t].address != nodeinfo.address) {
-			printf("%12d %12d %12d %12d", (int) NL_table[t].address,
+		if (NL_table[t].src != nodeinfo.src) {
+			printf("%12d %12d %12d %12d", (int) NL_table[t].src,
 					NL_table[t].ackexpected, NL_table[t].nextpackettosend,
 					NL_table[t].packetexpected);
 			if (NL_table[t].minhop_link != 0)
@@ -153,6 +141,7 @@ static EVENT_HANDLER( show_NL_table) {
 			//}
 		}
 }
+*/
 
 void reboot_NL_table(void) {
 	CHECK(CNET_set_handler(EV_DEBUG0, show_NL_table, 0));
--- trunk/milestone_3_final2/receive_buffer.c
+++ trunk/milestone_3_final2/receive_buffer.c
@@ -4,17 +4,17 @@ typedef struct {
   unsigned int id;
   size_t length;
   char msg[MAX_MESSAGE_SIZE];
-} BUF_DATA
+} BUF_DATA;
 
-typedef struct {
+struct BUF_NODE {
   BUF_DATA *data;
-  BUF_DATA *next;
-} BUF_NODE;
+  struct BUF_NODE *next;
+};
 
-static BUF_NODE *rb = NULL;
+static struct BUF_NODE *rb = NULL;
 
 unsigned int get_id (NL_PACKET *p){
-  return (unsigned int)(p->src) * 10000000 + (unsigned int) (p->dest) * 10000 + (unsigned int) seqno;
+  return ((unsigned int) (p->src) * 10000000 + (unsigned int) (p->dest) * 10000 + (unsigned int) p->seqno);
 }
 
 void RB_save_msg (NL_PACKET *p){
@@ -22,18 +22,20 @@ void RB_save_msg (NL_PACKET *p){
   unsigned int id = get_id(p);
   
   if (rb == NULL) {
-    rb = malloc(sizeof(BUF_NODE));
+    rb = malloc(sizeof(struct BUF_NODE));
     rb->data->id = id;
     memcpy(&rb->data->msg[0], (char *) p->msg, p->length);
     rb->data->length = p->length;
     rb->next = NULL;
+    printf("receive buffer initialized");
   } 
   else {
-    BUF_NODE *temp = rb;
+    struct BUF_NODE *temp = rb;
     while (temp->next != NULL){
       if(temp->data->id == id){
-	memcpy(&temp->data->msg[length], (char *) p->msg, p->length);
+	memcpy(&temp->data->msg[temp->data->length], (char *) p->msg, p->length);
 	temp->data->length += p->length;
+	printf("node existed in recevier buffer");
 	break;
       }
       temp = temp->next;
@@ -41,32 +43,33 @@ void RB_save_msg (NL_PACKET *p){
     //save at rear node or create a new node
     if(temp->next == NULL){
       if(temp->data->id == id){
-	memcpy(&temp->data->msg[length], (char *) p->msg, p->length);
+	memcpy(&temp->data->msg[temp->data->length], (char *) p->msg, p->length);
 	temp->data->length += p->length;
       } else {
-	BUF_NODE *newNode = malloc(sizeof(BUF_NODE));
+	struct BUF_NODE *newNode = malloc(sizeof(struct BUF_NODE));
 	newNode->data->id = id;
 	memcpy(newNode->data->msg, (char *) p->msg, p->length);
 	newNode->data->length = p->length;
 	newNode->next = NULL;
 	temp->next = newNode;
+	printf("new node created in receive buffer");
       }
     }
   }
 }
 
-void RB_get_whole_msg(NL_PACKET *p){
+void RB_copy_whole_msg(NL_PACKET *p){
   /* UINT_MAX = +4,294,967,295 */
   unsigned int id = get_id(p);
   
-  BUF_NODE *temp = rb
+  struct BUF_NODE *temp = rb;
   if(temp->data->id == id){
     memcpy(p->msg, temp->data->msg, temp->data->length);
     rb = rb->next;
     free(&temp->data->msg);
     free(temp);
   } else{
-    BUF_NODE *prev = rb;
+    struct BUF_NODE *prev = rb;
     temp = temp->next;
     while(temp != NULL){
       if(temp->data->id == id){
