8c417f3c805655858608d66979b3d9c47eb25e62
cn.jihao@gmail.com <cn.jihao@gmail.com@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-09-27 07:45:34 +0000

    intial milestone_3_final2, reimplement receive_buffer

---
+++ trunk/milestone_3_final2/milestone3.h
@@ -0,0 +1,27 @@
+#ifndef _MILESTONE3_H_
+#define _MILESTONE3_H_
+
+#include "nl_packet.h"
+
+#define PACKET_HEADER_SIZE  (sizeof(NL_PACKET) - MAX_MESSAGE_SIZE)
+#define PACKET_SIZE(p)      (PACKET_HEADER_SIZE + p.length)
+#define MAXHOPS             10
+
+//static char receiveBuffer[256][MAX_MESSAGE_SIZE];
+//static char *rb[256];
+//static size_t packet_length[256];
+NL_PACKET * lastPacket;
+static int mtu;
+
+extern void send_ack(NL_PACKET *p, int arrived_on_link,
+		unsigned short int is_err_ack);
+extern void print_msg(char *msg, size_t length);
+extern void update_last_packet(NL_PACKET *last);
+extern NL_PACKET * get_last_packet(CnetAddr address);
+extern void sdown_pieces_to_datalink(char *packet, size_t length,
+		int choose_link);
+extern int up_to_network(char *packet, size_t length, int arrived_on_link);
+extern void up_to_application(NL_PACKET *p, int arrived_on_link);
+extern void route_packet(NL_PACKET *p, int arrived_on_link);
+
+#endif
