e868230f15768e161ecf45ffc1e1dc3d1d6747ad
cn.jihao@gmail.com <cn.jihao@gmail.com@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-09-27 15:19:07 +0000

    fds

--- milestone_3_final2/nl_packet.h
+++ milestone_3_final2/nl_packet.h
@@ -1,14 +1,21 @@
 #ifndef _NL_PACKET_H_
 #define _NL_PACKET_H_
-#include <cnet.h>
+//#include <cnet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_MESSAGE_SIZE 1000
 
 typedef enum {
 	NL_DATA, NL_ACK, NL_ERR_ACK, NL_ERR_ACK_RESENT
 } NL_PACKETKIND;
 
 typedef struct {
-	CnetAddr src;
-	CnetAddr dest;
+	//CnetAddr src;
+	//CnetAddr dest;
+	int src;
+	int dest;
 	NL_PACKETKIND kind; /* only ever NL_DATA or NL_ACK */
 	unsigned short int seqno; /* 0, 1, 2, ... */
 	unsigned short int hopcount;
