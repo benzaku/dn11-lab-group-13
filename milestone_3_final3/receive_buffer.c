69aaea31bc6acf91bb12b6ffd969f17cbf1469c9
zhe.fang21@gmail.com <zhe.fang21@gmail.com@2367b34a-9cc4-f26a-3eec-994847ccf6bc>
2011-10-01 00:19:11 +0000

    filling mode for msg saving in buffer and some simplifications in miletone3.c

--- milestone_3_final3/receive_buffer.c
+++ milestone_3_final3/receive_buffer.c
@@ -10,11 +10,6 @@ typedef struct {
 size_t RB_ELEM_SIZE = sizeof(RB_BUF_ELEM);
 
 
-unsigned int RB_get_id(NL_PACKET *p) {
-	return ((unsigned int) (p->src) * 10000000 + (unsigned int) (p->dest)
-			* 10000 + (unsigned int) p->seqno);
-}
-
 unsigned int RB_get_id_link(NL_PACKET *p, int arrive_on_link){
 	unsigned int hashPart = (unsigned int) (p->src * 13) + (unsigned int) (p->dest * 7) + (unsigned int) p->seqno;
 	return (unsigned int) 100 * hashPart + (unsigned int) arrive_on_link;
@@ -26,85 +21,51 @@ void RB_init(VECTOR rb) {
 	rb = vector_new();
 }
 
+//if success return 1, if full return 2
 int RB_save_msg_link(VECTOR rb, NL_PACKET *p, int arrive_on_link) {
-	if(p->length > MAX_MESSAGE_SIZE || p->length <= 0)
-	  return 0;
-	/*
-	printf("RB_save_msg\n");
-	printf("packet to be saved: src = %d, des = %d, seqno = %d\n, current = %d ", p->src,
-			p->dest, p->seqno, nodeinfo.address);
-	*/
+	
 	RB_BUF_ELEM* temp;
 	RB_BUF_ELEM bufelem;
 	unsigned int id = RB_get_id_link(p, arrive_on_link);
-	//printf("calc_id = %d\n", id);
+        
 	int i;
 	int n = vector_nitems(rb);
-	if (n == 0) {
-		//temp = malloc(RB_ELEM_SIZE);
-		//temp->id = id;
-		//temp->length = p->length;
-		//memcpy(temp->msg, (char *) p->msg, p->length);
-		  bufelem.id = id;
-		  bufelem.length = p->length;
-		  memcpy(bufelem.msg, (char *) p->msg, p->length);
-		  vector_append(rb, &bufelem, RB_ELEM_SIZE);
-		  return 1;
-	} else {
-		for (i = 0; i < n; i++) {
-			temp = vector_peek(rb, i, &RB_ELEM_SIZE);
-			if (temp->id == id) {
-				//backup to new elem because of vector_replace
-				if(temp->length + p->length <= MAX_MESSAGE_SIZE){
-				  memcpy(&temp->msg[temp->length], (char *) p->msg, p->length);
-				  temp->length += p->length;
-				  return 1;
-				} else {
-				  return 0;
-				}
-				/*
-				RB_BUF_ELEM elem;
-				elem.id = id;
-				elem.length = temp->length;
-				memcpy(&elem.msg, &temp->msg, elem.length);
-				memcpy(&elem.msg[elem.length], (char *) p->msg, p->length);
-
-				elem.length += p->length;
-				vector_replace(rb, i, &elem, RB_ELEM_SIZE);
-				temp = vector_peek(rb, i, &RB_ELEM_SIZE); // debug
-				*/
-				/*
-				printf(
-						"elem found: msg saved at vector[%d], msg_id= %d, msg_length = %d\n",
-						i, temp->id, temp->length);
-				printf("p->isend = %d", p->pieceEnd);
-				*/
-				break;
-			}
-		}
-		if (i == n) {
-			  RB_BUF_ELEM tempelem;
-			  tempelem.id = RB_get_id_link(p, arrive_on_link);
-			  tempelem.length = p->length;
-			  memcpy(tempelem.msg, (char *) p->msg, p->length);
-			  vector_append(rb, &tempelem, RB_ELEM_SIZE);
-		}
-	}
-	return 1;
-	//printf("\n");
+        
+	
+        for (i = 0; i < n; i++) {
+                temp = vector_peek(rb, i, &RB_ELEM_SIZE);
+                if (temp->id == id) {
+                    //backup to new elem because of vector_replace
+                        
+                    memcpy(&temp->msg[p->pieceStartPosition], (char *) p->msg, p->length);
+                    temp->length += p->length;
+                    if(temp->length == p->src_packet_length) return 2;
+                    else return 1;
+                
+                
+                }
+        }
+        
+        //id not found, created new entry in vector
+        if (i == n) {
+                
+            bufelem.id = id;
+            bufelem.length = p->length;
+            memcpy(bufelem.msg, (char *) p->msg, p->length);
+            vector_append(rb, &bufelem, RB_ELEM_SIZE);
+            return 1;
+        }
+	return 0;
 }
 
 void RB_copy_whole_msg_link(VECTOR rb, NL_PACKET *p, int arrive_on_link) {
-	/*
-	printf("RB_copy_whole_msg\n");
-	printf("packet to be removed: src = %d, des = %d, seqno = %d\n, current = %d ", p->src,
-			p->dest, p->seqno, nodeinfo.address);
-	*/
-	unsigned int id = RB_get_id_link(p,arrive_on_link);
+	
+	unsigned int id = RB_get_id_link(p, arrive_on_link);
 	unsigned int hashPart = id / 100;
-	//printf("calc_id = %d\n", id);
-	int i;
+	
+        int i;
 	int n = vector_nitems(rb);
+        
 	for (i = 0; i < n; i++) {
 		RB_BUF_ELEM *temp;
 		temp = vector_peek(rb, i, &RB_ELEM_SIZE);
@@ -128,51 +89,4 @@ void RB_copy_whole_msg_link(VECTOR rb, NL_PACKET *p, int arrive_on_link) {
 
 	}
 
-	//printf("\n");
 }
-
-/*
-int isCorrupted(NL_PACKET * p) {
-	int p_checksum = p->checksum;
-	int checksum = CNET_ccitt((unsigned char *) p->msg,
-			p->src_packet_length);
-	if(p_checksum == checksum){
-		return 1;
-	}
-	return 0;
-}
-*/
-
-/*
-void new_RB_save_msg(NL_PACKET *p) {
-	RB_BUF_ELEM * buf_elem_p;
-	RB_BUF_ELEM buf_elem;
-	size_t size = sizeof(RB_BUF_ELEM);
-	int n = vector_nitems(rb);
-	int i = 0;
-	unsigned int id = RB_get_id(p);
-
-	if (n == 0) {
-		//no element
-		buf_elem.id = id;
-		buf_elem.length = p->length;
-		memcpy(&(buf_elem.msg), (char *) &(p->msg), p->length);
-		vector_append(rb, &buf_elem, sizeof(buf_elem));
-	} else {
-		for (i = 0; i < n; i++) {
-			buf_elem_p = vector_peek(rb, i, &size);
-			if (buf_elem_p->id == id) {
-				memcpy(&(buf_elem_p->msg[buf_elem_p->length]), &(p->msg),
-						p->length);
-				buf_elem_p->length += p->length;
-				return;
-			}
-		}
-		//there is no node in vector.
-		buf_elem.id = id;
-		buf_elem.length = p->length;
-		memcpy(&(buf_elem.msg), (char *) &(p->msg), p->length);
-		vector_append(rb, &buf_elem, sizeof(buf_elem));
-	}
-}
-*/
